c77cfcb41343265d933923fa0a50d91865fdbfa9
Vladimir Polyakov <nrd11k@gmail.com>
2015-05-04 00:37:40 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var through = require('through2');
+var path = require('path');
+var reactJade = require('react-jade');
+var gutil = require('gulp-util');
+var PluginError = gutil.PluginError;
+
+var PLUGIN_NAME = 'gulp-react-jade-amd';
+
+function gulpReactJadeAmd(options) {
+	return through.obj(function(file, enc, callback) {
+		if (file.isNull()) {
+			this.push(file);
+			return callback();
+		}
+		if (file.isBuffer()) {
+			var templateString = reactJade.compileClient(
+				file.contents.toString(),
+				options
+			).toString();
+			file.contents = new Buffer(
+				'define([\'react\'], function(React) {return ' + templateString + ';});'
+			);
+			file.path = path.dirname(file.path) + '/' +
+				path.basename(file.path, path.extname(file.path)) + '.js';
+			this.push(file);
+			return callback();
+		}
+		if (file.isStream()) {
+			this.emit('error', new PluginError(PLUGIN_NAME, 'Streams are not supported!'));
+			return callback();
+		}
+
+		return callback();
+	});
+};
+
+module.exports = gulpReactJadeAmd;
